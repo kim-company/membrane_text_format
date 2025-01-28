987766af24692fe29f491e09b33f9c49e3125fd6
Daniel Morandini <danielmorandini@me.com>
2025-01-28 11:17:29 +0100

    Remove locale field
    In the context of S2T or T2TT, language might
    change on a per-buffer basis. For this reason,
    we've removed the language field from this spec.

--- lib/membrane/text.ex
+++ lib/membrane/text.ex
@@ -1,9 +1,7 @@
 defmodule Membrane.Text do
   @type t() :: %__MODULE__{
           encoding: :utf8,
-          locale: String.t()
         }
 
-  @enforce_keys [:locale]
-  defstruct [encoding: :utf8] ++ @enforce_keys
+  defstruct [encoding: :utf8]
 end
